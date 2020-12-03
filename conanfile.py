db927ed89c37b253932e2cc32373c4a89851aeae
Andrew Marshall <planetmarshalluk@gmail.com>
2020-12-03 22:47:04 +0000

    update conanfile and cmake config

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,3 @@
-from os import path
-
 from conans import (
     ConanFile,
     CMake,
@@ -14,32 +12,27 @@ class SampleConan(ConanFile):
     description = 'Sample C++ Project using Conan and CMake'
     license = 'https://www.apache.org/licenses/LICENSE-2.0'
     settings = 'build_type', 'compiler', 'os', 'arch'
-    generators = 'cmake'
-    requires = (
-        'gtest/1.8.1@bincrafters/stable'
+    generators = 'cmake','cmake_find_package'
+    build_requires = (
+        'gtest/1.8.1'
     )
-    staging_dir = 'conan_stage'
 
     def source(self):
         git = tools.Git()
         git.clone('git@github.com:planetmarshall/cpp_sample_project.git')
 
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
 
     def build(self):
-        cmake = CMake(self)
-        cmake_definitions = {
-            'CMAKE_INSTALL_PREFIX': self.staging_dir
-        }
-        cmake.definitions.update(cmake_definitions)
-        cmake.configure()
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        self.copy('*', src=self.staging_dir)
-
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ['sample_core']
-
-
