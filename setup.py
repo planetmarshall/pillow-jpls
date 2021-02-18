8b979e20ed3b1f38b9376fb78515cb98e272a01c
Andrew Marshall <planetmarshalluk@gmail.com>
2021-02-18 18:25:28 +0000

    add setuptools info

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import find_packages
+
+try:
+    from skbuild import setup
+except ImportError:
+    print(
+        "Please update pip, you need pip 10 or greater,\n"
+        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        file=sys.stderr,
+    )
+    raise
+
+setup(
+    name="pillow-jpls",
+    version="0.0.1",
+    description="A JPEG-LS plugin for the Pillow imaging library",
+    author="Andrew Marshall",
+    license="MIT",
+    packages=find_packages(),
+    cmake_install_dir="src/"
+)
