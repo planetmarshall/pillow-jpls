1f224ad607903d6092e3f5fe8cad2fd31fbb3809
Andrew Marshall <planetmarshalluk@gmail.com>
2021-02-18 18:13:28 +0000

    add skeleton tests

---
+++ test/test_conformance.py
@@ -0,0 +1,30 @@
+import pillow_jpls
+
+from PIL import Image
+
+def test_compress_lossless_no_interleave():
+    assert 0
+
+def test_compress_lossless_line_interleave():
+    assert 0
+
+def test_compress_lossless_sample_interleave():
+    assert 0
+
+def test_compress_lossy_no_interleave():
+    assert 0
+
+def test_compress_lossy_line_interleave():
+    assert 0
+
+def test_compress_lossy_sample_interleave():
+    assert 0
+
+def test_compress_lossless_subsample_line_interleaved():
+    assert 0
+
+def test_compress_lossy_subsample_line_interleaved():
+    assert 0
+
+def test_decompress():
+    assert 0
