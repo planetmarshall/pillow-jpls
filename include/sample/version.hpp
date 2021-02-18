ab632cd863d3f1c58148d35bd9bb04eebda643cc
Andrew Marshall <planetmarshalluk@gmail.com>
2021-02-18 17:05:22 +0000

    remove unused folders

--- include/sample/version.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace sample {
-    std::string version();
-}
--- test/core/test_version.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include <sample/version.hpp>
-
-TEST(TestVersion, VersionIsNotNull) {
-    ASSERT_EQ("1.0.0", sample::version());
-}
