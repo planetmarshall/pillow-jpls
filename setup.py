dfd78bbbf94f7e26534a1b3725bbc6cede75ae06
Andrew Marshall <planetmarshalluk@gmail.com>
2021-02-26 20:06:07 +0000

    add additional metadata

--- setup.py
+++ setup.py
@@ -16,10 +16,13 @@ setup(
     version="0.0.1",
     description="A JPEG-LS plugin for the Pillow imaging library",
     author="Andrew Marshall",
+    author_email="andrew@algodynamic.com",
+    url="https://github.com/planetmarshall/pillow-jpls",
     license="BSD-3-Clause",
     packages=find_packages(),
     cmake_install_dir="pillow_jpls",
     install_requires=[
         "Pillow"
-    ]
+    ],
+    python_requires=">=3.6"
 )
